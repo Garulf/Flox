9f9ca16af41ae365473e5b785778fe4adc656729
Garulf <535299+Garulf@users.noreply.github.com>
2021-07-30 06:48:55 -0400

    add lib at plugin path to sys path on import

--- flox/__init__.py
+++ flox/__init__.py
@@ -1,1 +1,29 @@
+import sys
+import os
+
 from .flox import Flox
+
+PLUGIN_MANIFEST = 'plugin.json'
+
+potential_paths = [
+    os.path.abspath(os.getcwd()),
+    os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+]
+
+for path in potential_paths:
+
+    while True:
+        if os.path.exists(os.path.join(path, PLUGIN_MANIFEST)):
+            plugindir = path
+            break
+        elif path == '/':
+            break
+
+        path = os.path.dirname(path)
+
+    if plugindir:
+        break
+
+lib_path = os.path.join(plugindir, 'lib')
+if os.path.exists(lib_path):
+    sys.path.append(lib_path)
