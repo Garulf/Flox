755fa24b2d9efd94c9e1e777529210903cf068c3
Garulf <535299+Garulf@users.noreply.github.com>
2022-01-19 03:55:47 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 from setuptools import setup
 
-setup(name='Flox',
-      version='0.9.9',
-      description='Python library to help build Flow Launcher and Wox plugins',
-      url='https://github.com/Garulf/Flox',
+URL = 'https://github.com/Garulf/Flox'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("./flox/version", "r") as f:
+    version = f.read().strip()
+
+setup(name='Flox-lib',
+      version=version,
+      description='Python library to help build Flow Launcher and Wox plugins.',
+      long_description=long_description,
+      url=URL,
+      project_urls={
+              "Bug Tracker": f"{URL}/issues"
+      },
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Windows",
+      ],
       author='William McAllister',
       author_email='dev.garulf@gmail.com',
       license='MIT',
-      packages=['flox-lib'],
+      packages=['flox'],
       zip_safe=False)
