5662973ed480c90240be5d642233d5d54f4b6695
Garulf <535299+Garulf@users.noreply.github.com>
2022-04-06 06:10:39 -0400

    Move settings to seperate file

--- flox/__init__.py
+++ flox/__init__.py
@@ -15,6 +15,7 @@ from tempfile import gettempdir
 
 from .launcher import Launcher
 from .browser import Browser
+from .settings import Settings
 
 PLUGIN_MANIFEST = 'plugin.json'
 FLOW_LAUNCHER_DIR_NAME = "FlowLauncher"
@@ -320,54 +321,3 @@ class Flox(Launcher):
     @cached_property
     def python_dir(self):
         return self.app_settings["PluginSettings"]["PythonDirectory"]
-
-class Settings(dict):
-
-    def __init__(self, filepath):
-        super(Settings, self).__init__()
-        self._filepath = filepath
-        self._save = True
-        if os.path.exists(self._filepath):
-            self._load()
-        else:
-            data = {}
-            self.update(data)
-            self.save()
-
-        
-    def _load(self):
-        data = {}
-        with open(self._filepath, 'r') as f:
-            try:
-                data.update(json.load(f))
-            except json.decoder.JSONDecodeError:
-                pass
-
-        self._save = False
-        self.update(data)
-        self._save = True
-
-    def save(self):
-        if self._save:
-            data = {}
-            data.update(self)
-            with open(self._filepath, 'w') as f:
-                json.dump(data, f, sort_keys=True, indent=4)
-        return
-    
-    def __setitem__(self, key, value):
-        super(Settings, self).__setitem__(key, value)
-        self.save()
-
-    def __delitem__(self, key):
-        super(Settings, self).__delitem__(key)
-        self.save()
-
-    def update(self, *args, **kwargs):
-        super(Settings, self).update(*args, **kwargs)
-        self.save()
-
-    def setdefault(self, key, value=None):
-        ret = super(Settings, self).setdefault(key, value)
-        self.save()
-        return ret
