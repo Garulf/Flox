3eb3e945f36ef22f23c72a381dbfa3d0fc29b296
Garulf <535299+Garulf@users.noreply.github.com>
2022-04-22 08:40:12 -0400

    Restore __del__ method

--- flox/launcher.py
+++ flox/launcher.py
@@ -12,7 +12,7 @@ class Launcher(object):
     Launcher python plugin base
     """
 
-    def __call__(self, debug=None):
+    def __del__(self, debug=None):
         self.run(debug)
 
     def run(self, debug=None):
